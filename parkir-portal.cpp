de18969e4cfdc3357f42a4e977b6c8144a6cea10
twn356 <syarifah.najwa.alaydrus@gmail.com>
2025-06-19 11:46:18 +0800

    menambahkan void tampilkanMenu

--- parkir-portal.cpp
+++ parkir-portal.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int main () {
-    cout << "hello world";
-
-    return 0;
-}
+void tampilkanMenu(){
+    cout << "\n === Sistem Parkir Portal ===\n";
+    cout << "1. Tampilkan kartu\n";
+    cout << "2. Hapus kartu\n";
+    cout << "3. Daftar lahan parkir kosong\n";
+    cout << "4. Keluar dari parkir\n";
+    cout << "5. Keluar dari program\n";
+}s
