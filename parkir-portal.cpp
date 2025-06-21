7fe619b9034c451143ae342dc40eedd5c9b7548d
Mmeis <meisyandaryani@gmail.com>
2025-06-21 11:17:46 +0000

    penambahan class sistemParkir, include vector, dan perbaikan void tampilkanMenu

--- parkir-portal.cpp
+++ parkir-portal.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 #include <ctime>
 #include <thread>
+#include <vector>
 using namespace std;
 
 struct parkir{
@@ -10,18 +11,29 @@ struct parkir{
     time_t waktuMasuk;
 };
 
+class sistemParkir{
+private:
+    unordered_map<string , parkir> parkirAktif;
+    vector<int> lahanKosong;
+    int kapasitas;
+public:
+    sistemParkir(int kapasitas) : kapasitas(kapasitas){
+        for (int i = 1; i <= kapasitas; i++){
+            lahanKosong.push_back(i);
+        }
+    }
 
-void tampilkanMenu(){
-    cout << "\n === Sistem Parkir Portal ===\n";
-    cout << "1. Tampilkan kartu\n";
-    cout << "2. Hapus kartu\n";
-    cout << "3. Daftar lahan parkir kosong\n";
-    cout << "4. Keluar dari parkir\n";
-    cout << "5. Keluar dari program\n";
-}
+    void tampilkanMenu(){
+        cout << "\n === Sistem Parkir Portal ===\n";
+        cout << "1. Tampilkan kartu\n";
+        cout << "2. Hapus kartu\n";
+        cout << "3. Daftar lahan parkir kosong\n";
+        cout << "4. Keluar dari parkir\n";
+        cout << "5. Keluar dari program\n";
+    }
+};
 
-int main(){
-
+int main () {
 
     return 0;
 }
